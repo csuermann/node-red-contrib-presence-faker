5c84b4989b56173123a36bb2d4f23a20d9549dbf
cornelius <cornelius.suermann@gmail.com>
2019-01-22 22:05:07 +0100

    calculate random off durations

--- presence-faker.js
+++ presence-faker.js
@@ -4,36 +4,66 @@ const random = require('random')
 let myIntervalId
 
 function createSchedule (node, config) {
-  const now = dayjs()
+  // const now = dayjs()
 
-  const windowBegin = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowBegin)
-  const windowEnd = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowEnd)
+  // const windowBegin = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowBegin)
+  // const windowEnd = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowEnd)
 
-  const windowDuration = windowEnd.diff(windowBegin, 'minutes')
+  // const windowDuration = windowEnd.diff(windowBegin, 'minutes')
 
-  const calcRandomDuration = () =>
-    random.int(Number(config.minDuration), Number(config.maxDuration))
+  // const calcRandomDuration = () =>
+  //   random.int(Number(config.minDuration), Number(config.maxDuration))
 
-  const calcRandomCount = () =>
-    random.int(Number(config.minCount), Number(config.maxCount))
+  // const calcRandomCount = () =>
+  //   random.int(Number(config.minCount), Number(config.maxCount))
 
-  const count = calcRandomCount()
-  node.warn('randomCount: ' + count)
+  // const count = calcRandomCount()
+  // node.warn('randomCount: ' + count)
 
-  const blocks = Array()
+  const onBlocks = []
+  const offBlocks = []
 
-  for (let i = 1; i <= count; i++) {
-    let block = {
-      blockId: i,
-      duration: calcRandomDuration()
-    }
+  // for (let i = 1; i <= count; i++) {
+  //   let block = {
+  //     on: true,
+  //     duration: calcRandomDuration()
+  //   }
 
-    blocks.push(block)
-    node.warn(block)
+  //   onBlocks.push(block)
+  // }
+
+  // let sumOnDurations = onBlocks.reduce((prev, curr) => prev + curr.duration, 0)
+  // node.warn(`SUM ON: ${sumOnDurations} minutes`)
+
+  // REMOVE
+  count = 4
+  sumOnDurations = 10
+  windowDuration = 60
+
+  const calcDeviation = avgDeviation => {
+    return random.int(-1 * avgDeviation, avgDeviation)
   }
 
-  let sumDurations = blocks.reduce((prev, curr) => prev + curr.duration, 0)
-  node.warn(`SUM: ${sumDurations} minutes`)
+  // how much time is left for OFF blocks?
+  let sumOffDurations = windowDuration - sumOnDurations
+  sumOffDurations < 0 ? 0 : sumOffDurations
+
+  let avgOffDuration = Math.ceil(sumOffDurations / count + 1)
+  let deviation = Math.floor(avgOffDuration * 0.1)
+  let lastDeviation = 0
+
+  for (let i = count + 1; i > 0; i--) {
+    let currentDeviation = i > 1 ? calcDeviation(deviation) : 0
+    let block = {
+      on: false,
+      duration: avgOffDuration + currentDeviation + -1 * lastDeviation
+    }
+
+    offBlocks.push(block)
+    node.warn(block)
+
+    lastDeviation = currentDeviation
+  }
 
   // node.send(msg)
 
