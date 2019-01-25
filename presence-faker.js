7df9e8e51221a4171d055c60f0b84fc62a99cf29
cornelius <cornelius.suermann@gmail.com>
2019-01-25 18:39:04 +0100

    fake schedule cron each day

--- presence-faker.js
+++ presence-faker.js
@@ -2,84 +2,102 @@ const { createSchedule, stripPastBlocks } = require('./pf')
 const CronJob = require('cron').CronJob
 const dayjs = require('dayjs')
 
-function cancelSchedule(node, config) {
-  node.send({
-    topic: config.offTopic,
-    payload: config.offPayload
-  })
-}
-
-module.exports = function(RED) {
-  function PresenceFakerNode(config) {
+module.exports = function (RED) {
+  function PresenceFakerNode (config) {
     RED.nodes.createNode(this, config)
-    const node = this
 
-    node.on('input', function(msg) {
-      const sendMsg = isOn => {
-        node.send({
-          topic: isOn ? config.onTopic : config.offTopic,
-          payload: isOn ? config.onPayload : config.offPayload
+    const node = this
+    let schedule = []
+    let msgCron
+
+    const scheduleNextMessage = function () {
+      let nextBlock = schedule.shift()
+
+      if (nextBlock) {
+        sendMsg(nextBlock.isOn)
+
+        const when = dayjs()
+          .add(2, 'second')
+          .toDate()
+        // const when = nextBlock.begin.toDate()
+
+        msgCron = new CronJob({
+          cronTime: when, // nextBlock.begin.toDate()
+          onTick: scheduleNextMessage
+        })
+
+        msgCron.start()
+
+        node.status({
+          fill: 'yellow',
+          shape: nextBlock.isOn ? 'dot' : 'ring',
+          text: `${nextBlock.isOn ? 'ON' : 'OFF'} [${nextBlock.beginString} - ${
+            nextBlock.endString
+          }]`
+        })
+      } else {
+        node.status({
+          fill: 'grey',
+          shape: 'dot',
+          text: 'suspended'
         })
       }
+    }
 
+    const sendMsg = isOn => {
+      node.send({
+        topic: isOn ? config.onTopic : config.offTopic,
+        payload: isOn ? config.onPayload : config.offPayload
+      })
+    }
+
+    let now = dayjs()
+    begin = dayjs(now.format('YYYY-MM-DD') + 'T' + config.windowBegin)
+
+    const fakeCronTime = dayjs()
+      .add(5, 'second')
+      .toDate()
+
+    let rescheduleCron = new CronJob({
+      // cronTime: `0 ${begin.minute()} ${begin.hour()} * * *`, // sec min hour dom month dow
+      cronTime: fakeCronTime,
+      onTick: () => {
+        schedule = createSchedule(config)
+        scheduleNextMessage()
+      }
+    })
+
+    rescheduleCron.start()
+
+    node.on('input', function (msg) {
       if (msg.payload === true) {
         // activate!
-        let schedule = stripPastBlocks(createSchedule(config))
+        schedule = stripPastBlocks(createSchedule(config))
 
-        if (schedule.length === 0) {
-          node.status({
-            fill: 'grey',
-            shape: 'dot',
-            text: 'schedule completed'
-          })
-        } else {
-          let cronjob
-          const scheduleNextMessage = function() {
-            let nextBlock = schedule.shift()
-            sendMsg(nextBlock.isOn)
+        scheduleNextMessage()
 
-            const when = dayjs()
-              .add(2, 'second')
-              .toDate()
-            // const when = nextBlock.begin.toDate()
-
-            cronjob = new CronJob({
-              cronTime: when, // nextBlock.begin.toDate()
-              onTick: scheduleNextMessage
-            })
-
-            cronjob.start()
-
-            node.status({
-              fill: 'yellow',
-              shape: nextBlock.isOn ? 'dot' : 'ring',
-              text: `${nextBlock.isOn ? 'ON' : 'OFF'} [${
-                nextBlock.beginString
-              } - ${nextBlock.endString}]`
-            })
-          }
-
-          scheduleNextMessage()
-        }
-
-        // node.warn(schedule)
-
-        let nodeContext = node.context()
-        nodeContext.set('schedule', schedule)
+        // let nodeContext = node.context()
+        // nodeContext.set('schedule', schedule)
       } else if (msg.payload === false) {
         // deactivate!
         node.status({
           fill: 'red',
           shape: 'ring',
-          text: 'suspended'
+          text: 'inactive'
         })
 
-        cancelSchedule(node, config)
+        if (msgCron) {
+          msgCron.stop()
+        }
       }
     })
 
-    node.on('close', function() {
-      // tidy up
+    node.on('close', function () {
+      rescheduleCron.stop()
+
+      if (msgCron) {
+        msgCron.stop()
+      }
     })
   }
 
